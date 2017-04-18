cce7b738686744d437aa10e80f16c83bbe2671a7
Tazeek <tazeek@binary.com>
2017-04-18 14:50:29 +0800

    Removing HTML tags

--- clean.py
+++ clean.py
@@ -1,11 +1,17 @@
 import pandas as pd 
-import numpy as np 
 import re
 import codecs
-import time
-import os
 import itertools
 
+from bs4 import BeautifulSoup
+
+# Cleaning comments 
+def cleaning(review):
+
+    # Remove HTML tags using beautiful soup
+    review = BeautifulSoup(review,"lxml").get_text()
+
+    return review
 
 # Read the csv file
 reviews_df = pd.read_csv('Reviews.csv')
@@ -15,4 +21,8 @@ clean_reviews = []
 
 # Loop through comments in 'Text' tag
 for i in range(0, 20):
-    print(reviews_df['Text'][i], "\n\n")
+
+    # Clean text here 
+    clean_review = cleaning(reviews_df['Text'][i])
+    
+    print(clean_review, "\n\n")
